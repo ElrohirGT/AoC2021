f533f77753c146e3a1b6b3b7df1c6338f8e0151f
ElrohirGT <elrohirgt@gmail.com>
2021-12-02 15:16:11 -0600

    Solved Day 1.1

--- AdventOfCode/Solutions/Year2021/Day01/Day01.cs
+++ AdventOfCode/Solutions/Year2021/Day01/Day01.cs
@@ -7,15 +7,27 @@ namespace AdventOfCode.Solutions.Year2021
 
     class Day01 : ASolution
     {
+        private string[] _lines;
 
         public Day01() : base(01, 2021, "")
         {
-
+            _lines = Input.SplitByNewline();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            if (_lines.Length <= 1)
+                return "0";
+
+            int previousIsLowerCount = 0;
+            int previousValue = int.Parse(_lines[0]);
+            for (int i = 1; i < _lines.Length; i++)
+            {
+                int currentValue = int.Parse(_lines[i]);
+                if (previousValue < currentValue)
+                    previousIsLowerCount++;
+            }
+            return previousIsLowerCount.ToString();
         }
 
         protected override string SolvePartTwo()
